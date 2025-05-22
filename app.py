2590ace6c891c590d2645f6b6b20d413e004d1b5
Ryuzaki5972 <singhash5972@gmail.com>
2025-05-22 15:15:00 +0530

    edited app.py

--- app.py
+++ app.py
@@ -83,5 +83,4 @@ if __name__ == '__main__':
             print("Could not load existing model. Will need to train a new one.")
     
     print("Starting Flask application...")
-    print("Open your browser and go to: http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
